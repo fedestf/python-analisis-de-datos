ad07749bcd16e27946bc381ab5bf4599771d4c8b
fedestf <fede520@live.com>
2020-12-19 21:23:59 -0300

    Create Matrices.py
    -

---
+++ Matrices.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+array1=np.arange(1,9) #creo array
+
+array1.shape=(2,4) #ordeno en un array de 2*4
+
+print (array1)
+
+#suma
+resultado=array1+array1
+print(resultado)
+#resta
+resultado=array1-array1
+print(resultado)
+#exponenciacion
+resultado=array1**array1
+print(resultado)
+#multiplicacion
+resultado=array1*array1
+print(resultado)
+#division
+resultado=array1/array1
+print(resultado)
