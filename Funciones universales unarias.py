a388511c2b2d448cc4f62ba4c8311526a17a6690
fedestf <fede520@live.com>
2021-01-04 15:05:42 -0300

    Update Funciones universales unarias.py
    -

---
+++ Funciones universales unarias.py
@@ -0,0 +1,9 @@
+import numpy as np 
+a=np.array([2,4,9], float)
+
+print(np.sqrt(a)) #raiz cuadrada
+print(np.exp(a)) #exponencial
+print(np.log(a)) #logartimo natural
+print(np.mean(a)) #media aritmetica estadistica
+
+##para buscar mas googlear funciones unarias de numpy
