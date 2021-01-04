5ca8e345eb4a8f912dc3893ba4a81fe3014db6a4
fedestf <fede520@live.com>
2021-01-04 14:58:08 -0300

    Create Algebra lineal.py
    --

---
+++ Algebra lineal.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+print("\n ------------------")#PRODUCTO DE ARRAYS
+a=np.array([1,2,3],float)
+b=np.array([0,1,1],float)
+
+print(np.dot(b,a)) 
+
+print("\n ------------------")#PRODUCTO DE MATRICES
+
+a1=np.array([[5,2], [4,8]],float)
+b1=np.array([[2,4], [5,3]],float)
+
+print(a1,"\n ------------------")
+print(b1,"\n ------------------")
+
+print(a1@b1) #multiplicador de matrices
+
+print("\n ------------------")#ARRAY N DIMENSIONAL
+
+a2=np.array([[0,1,4],[5,2,3],[1,4,8]], float)
+b2=np.array([2,3,5],float )
+
+print(a2,"\n ------------------")
+print(b2,"\n ------------------")
+
+print(b2@a2)
+
+print("\n ------------------") #DETERMINANTE DE UNA MATRIZ
+
+a3=np.array([[8,5],[3,4]])
+print(a3,"\n --------")
+
+print(np.linalg.det(a3))
+
+print("\n ------------------") #AUTO VALORES Y AUTO VECTORES
+
+vals,vecs=np.linalg.eig(a3)
+print(vals,"\n ------------------")
+print(vecs,"\n ------------------")
