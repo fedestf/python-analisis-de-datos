b42d3f86715676a435ec941163abd565685a2d3d
fedestf <fede520@live.com>
2021-01-15 19:19:02 -0300

    Create Dataframe.py

---
+++ Dataframe.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+data = {
+    'pais': ['Caracas','Guadalajara','La habana','Cancun','Guasdalito'],
+    'poblacion':[100000,200000,340000,210000,300000],
+    'casos': [6000,4000,35000,4300,3002]
+       }
+
+df = pd.DataFrame(data) #creo el dataframe a partir de la info ingresada
+print (df)
