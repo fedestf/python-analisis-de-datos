42feca2925394beb581e97e2d93b65c124bcf371
fedestf <fede520@live.com>
2021-01-24 17:45:04 -0300

    Update Transformaciones.py

--- Transformaciones.py
+++ Transformaciones.py
@@ -1,39 +1,43 @@
-import numpy as np 
-#-------------------------------------------------------
-arrayEnteros = np.random.randint(100,size=(4,4)) # creo una matriz aleatoria de 3x4 con valores de 0 a 100
+import numpy as np
+# -------------------------------------------------------
+# creo una matriz aleatoria de 3x4 con valores de 0 a 100
+arrayEnteros = np.random.randint(100, size=(4, 4))
 
 print(arrayEnteros)
 
-#-------------------------------------------------------
-transformacion=arrayEnteros.astype(float) #transformacion a float 
+# -------------------------------------------------------
+transformacion = arrayEnteros.astype(float)  # transformacion a float
 
-print (transformacion)
+print(transformacion)
 
-#-------------------------------------------------------
-arrayEnteros=arrayEnteros.reshape(4,4) #modifica las dimensiones del array
+# -------------------------------------------------------
+arrayEnteros = arrayEnteros.reshape(4, 4)  # modifica las dimensiones del array
 
 print(arrayEnteros)
 
-#-------------------------------------------------------
-arrayPlano=arrayEnteros.flatten() #crea un array unidimensional desde el original
+# -------------------------------------------------------
+# crea un array unidimensional desde el original
+arrayPlano = arrayEnteros.flatten()
 
 print(arrayPlano)
 
-#-------------------------------------------------------
-listaArray=arrayPlano.tolist() #convierte el array unidimensional a una lista
+# -------------------------------------------------------
+listaArray = arrayPlano.tolist()  # convierte el array unidimensional a una lista
 
 print(listaArray)
 
-#-------------------------------------------------------
-arraySeparado=np.split(arrayEnteros,4)#separa el array array plano en 4 arrays 
+# -------------------------------------------------------
+# separa el array array plano en 4 arrays
+arraySeparado = np.split(arrayEnteros, 4)
 
 print(arraySeparado)
 
-arrayConcatenado=np.concatenate((arraySeparado[0],arraySeparado[3])) # concatena el array separado
+arrayConcatenado = np.concatenate(
+    (arraySeparado[0], arraySeparado[3]))  # concatena el array separado
 
 print(arrayConcatenado)
 
-#-------------------------------------------------------
-arrayTranspuesto=arrayEnteros.T #creo la matriz transpuesta, las filas pasan a ser las columnas
+# -------------------------------------------------------
+# creo la matriz transpuesta, las filas pasan a ser las columnas
+arrayTranspuesto = arrayEnteros.T
 print(arrayTranspuesto)
-
